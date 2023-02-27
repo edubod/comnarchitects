ff0e3ea15f4f71b83ba75478b577fd649fe5f7e3
edubod <bodak.eduard@gmail.com>
2023-02-27 18:51:00 +0100

    add final gsap animation

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,6 +1,10 @@
+import loadAnimation from './utils/load-animation';
+import mouseFollower from './utils/mouse-follower';
 import scrollAnimation from './utils/scroll-animation';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   scrollAnimation();
+  mouseFollower();
+  loadAnimation();
 });
